94393e761ce9559e014f907ebadd3421103fe293
Ignas Bobinas <ignas.bobinas@devbridge.com>
2019-02-07 17:06:39 +0200

    integrate customer creation with backend service.

--- src/components/customers/form/CustomerForm.tsx
+++ src/components/customers/form/CustomerForm.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {RouteComponentProps} from "react-router";
+import {Redirect, RouteComponentProps} from "react-router";
 import {Typography} from "@material-ui/core";
 import {PrimaryButton} from "../../lib/buttons/PrimaryButton";
 import {
@@ -7,71 +7,99 @@ import {
     GeneralDetailsCustomerFormState
 } from "./general-details-form/CustomerGeneralDetailsForm";
 import {CustomerConsentFormState, CustomerConsentsForm} from "./consents-form/CustomerConsentsForm";
+import axios from "axios";
 
 interface CustomerFormRouteParams {
     id: string;
 }
 
-export interface CustomerFormState
-  extends GeneralDetailsCustomerFormState, CustomerConsentFormState {
+export type CustomerFormStateValue = GeneralDetailsCustomerFormState & CustomerConsentFormState;
 
+export interface CustomerFormState {
+  value: CustomerFormStateValue;
+  created: boolean;
 }
 
 export class CustomerForm extends Component<RouteComponentProps<CustomerFormRouteParams>, CustomerFormState> {
 
-    constructor(props: RouteComponentProps<CustomerFormRouteParams>) {
-        super(props);
-        this.state = {
-            firstName: '',
-            lastName: '',
-            birthDate: null,
-            address: '',
-            consents: {}
-        }
+  constructor(props: RouteComponentProps<CustomerFormRouteParams>) {
+    super(props);
+    this.state = {
+      value: {
+        firstName: '',
+        lastName: '',
+        birthDate: null,
+        address: '',
+        consents: {}
+      },
+      created: false
     }
+  }
 
-    submitHandler = (event: any) => {
-        console.log('Form submitted' + JSON.stringify(this.state));
-        event.preventDefault();
-    };
+  submitHandler = (event: any) => {
+    this.createCustomer();
 
-    changeHandler = (subFormValue: Partial<CustomerFormState>) => {
-        this.setState({
-            ...this.state,
-            ...subFormValue
-        });
-    };
+    event.preventDefault();
+  };
 
-    render(): React.ReactNode {
-        const existingCustomer = !!this.props.match.params.id;
+  changeHandler = (subFormValue: Partial<CustomerFormStateValue>) => {
+    this.setState({
+      value: {
+        ...this.state.value,
+        ...subFormValue
+      }
+    });
+  };
 
-        const formlabel = existingCustomer ? (
-          "Customer  " + this.props.match.params.id
+  render(): React.ReactNode {
+    const existingCustomer = !!this.props.match.params.id;
+
+    const formlabel = existingCustomer ? (
+      "Customer  " + this.props.match.params.id
+    ) : (
+      "New customer"
+    );
+
+    return (
+      this.state.created ? (
+        <Redirect to="/customer/list"/>
         ) : (
-          "New customer"
-        );
+        <div>
+          <Typography variant="headline" color="primary">
+            {formlabel}
+          </Typography>
+          <CustomerGeneralDetailsForm
+            value={this.state.value}
+            onChange={this.changeHandler}
+          />
+          <CustomerConsentsForm
+            value={this.state.value}
+            onChange={this.changeHandler}
+          />
 
-        return (
-          <div>
-              <Typography variant="headline" color="primary">
-                  {formlabel}
-              </Typography>
-              <CustomerGeneralDetailsForm
-                value={this.state}
-                onChange={this.changeHandler}
-              />
-              <CustomerConsentsForm
-                value={this.state}
-                onChange={this.changeHandler}
-              />
+          <form
+            noValidate
+            onSubmit={this.submitHandler}
+          >
+            <PrimaryButton type="submit">Submit</PrimaryButton>
+          </form>
+        </div>
+      )
+    );
+  }
 
-              <form
-                noValidate
-                onSubmit={this.submitHandler}
-              >
-                  <PrimaryButton type="submit">Submit</PrimaryButton>
-              </form>
-          </div>
-        );
-    }
+  private createCustomer() {
+    const formState = this.state.value;
+    const consentsDTO = Object.keys(formState.consents || {})
+      .map(key => ({
+        id: key,
+        allowed: formState.consents[key]
+      }));
+
+    const dto = { ...formState, consents: consentsDTO };
+
+    axios.post("http://localhost:3000/api/customer", dto)
+      .then(() => this.setState({created: true}))
+      .catch((error) => console.error(error));
+  }
 }
